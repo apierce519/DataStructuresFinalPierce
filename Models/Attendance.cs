dce30064ef47366f49e73ad24c358bdf4d124c58
Andrew Pierce <ajpierce1@dmacc.edu>
2023-12-08 18:01:44 -0600

    Attenace work

---
+++ Models/Attendance.cs
@@ -0,0 +1,16 @@
+﻿namespace DataStructuresFinalPierce.Models
+{
+    public class Attendance
+    {
+        public int AttendanceId { get; set; }
+        public string CourseId { get; set; }
+        public List<int> StudentId { get; set; }
+        public List<bool> StudentAttendance { get; set; }
+        public DateOnly Date { get; set; }
+        public bool IsOpen { get; set; }
+        public Attendance()
+        {
+            //Date = DateOnly.FromDateTime(DateTime.Now);
+        }
+    }
+}
---
+++ Repositories/AttendanceRepository.cs
@@ -0,0 +1,36 @@
+﻿using DataStructuresFinalPierce.Models;
+
+namespace DataStructuresFinalPierce.Repositories
+{
+    public class AttendanceRepository
+    {
+
+        public string CsvFilePath = "attendances.csv";
+        private static List<Attendance> Attendances = new List<AttendanceRepository>();
+
+        public AttendanceRepository()
+        {
+            LoadDataFromCsv();
+        }
+
+        public void LoadDataFromCsv()
+        {
+            Attendances.Clear();
+            if (File.Exists(CsvFilePath))
+            {
+                Attendances.Clear();
+                var lines = File.ReadAllLines(CsvFilePath);
+                foreach (var line in lines.Skip(1))
+                {
+                    var values = line.Split(',');
+                    var attendance = new Attendance
+                    {
+                        AttendanceId = int.Parse(values[0]),
+                        CourseId = values[1],
+
+                    };
+                }
+            }
+        }
+    }
+}
